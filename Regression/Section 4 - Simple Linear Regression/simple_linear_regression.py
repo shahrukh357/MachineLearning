59ffe44a4950d5fac06ba73d78fb01d1cf29d012
shahrukh357 <37707810+shahrukh357@users.noreply.github.com>
2019-01-03 10:31:02 +0530

    Update simple_linear_regression.py

--- Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -14,15 +14,7 @@ y = dataset.iloc[:, 1].values
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train.reshape(-1,1))
-y_test = sc_y.fit_transform(y_test.reshape(-1,1))
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
